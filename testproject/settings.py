0c6f4ec0b6547455b11214d203579ec44c8ef178
ielboentoro <142801614+ielboentoro@users.noreply.github.com>
2024-04-16 13:08:21 +0700

    Update settings.py

--- testproject/settings.py
+++ testproject/settings.py
@@ -129,7 +129,7 @@ STATICFILES_DIRS = [
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = BASE_DIR / 'uploads' // os.path.join(BASE_DIR, "media")
 MEDIA_URL = '/media/'
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
